49f6b7523a23d74cdb20cf9fa2b423619ecc6f67
johirjoy <joy.sd69@gmail.com>
2023-06-26 01:02:32 +0600

    payment-intent post method API create and set

--- index.js
+++ index.js
@@ -170,6 +170,20 @@ async function run() {
       res.send(result);
     });
 
+    // create payment intent
+    app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
 
 
 
