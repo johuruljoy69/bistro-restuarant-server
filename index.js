3813afeb39b9b2cb3b2ce0039bdd9de09549a387
johirjoy <joy.sd69@gmail.com>
2023-06-26 00:54:25 +0600

    menu delete api set

--- index.js
+++ index.js
@@ -96,6 +96,13 @@ async function run() {
       const newItem = req.body;
       const result = await menuCollection.insertOne(newItem)
       res.send(result);
+    });
+
+    app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
